9a2c3d036d5a6a94f352af9c0b0e8abae3f81535
CoffinCoding <santoshpanthi678.com>
2024-08-06 11:45:53 +0545

    this is About form validation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::view('form','form');
+
+Route::post('details', [UserController::class, 'getUser']);
