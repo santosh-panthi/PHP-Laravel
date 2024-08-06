2ec9a35ff223729f94d2efaabd7448f81d126364
CoffinCoding <santoshpanthi678.com>
2024-08-07 00:32:02 +0545

    MySql connection without migration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 
 {
     function getUser()  {
-        return DB::select('select  * from users ');
+        return DB::select('select  * from details ');
 
     }
     //
