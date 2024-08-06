0d03f863f56be6923e139934d648d78514e523fb
CoffinCoding <santoshpanthi678.com>
2024-08-07 01:08:37 +0545

    get data using Eloquent Model ORM

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 
 {
-    function getUser()  {
-       $details =DB::select('select  * from details ');
-       return view('user',['details' => $details]);
-
+    function getUser()
+    {
+        $details = \App\Models\Detail::all();
+        return view('detail', ['details' => $details]);
     }
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('user',[UserController::class, "getUser"]);
+Route::get('detail',[UserController::class, "getUser"]);
